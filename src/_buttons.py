ee228b2dd9d40117bb5b0cd6d5c2ec12360c5d3d
DorLex <dorozhinskiysanya@gmail.com>
2025-11-07 21:49:23 +0300

    feat: update

--- src/_buttons.py
+++
@@ -1,7 +0,0 @@
-# _button_result = KeyboardButton('Показать результат')
-# _button_table_records = KeyboardButton('Показать таблицу рекордов')
-# _button_reset_score = KeyboardButton('Сыграть заново')
-#
-# end_menu = (
-#     ReplyKeyboardMarkup(resize_keyboard=True).add(_button_result).add(_button_table_records).add(_button_reset_score)
-# )
